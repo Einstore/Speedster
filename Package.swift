dd79c4bc51aa4de5981492685502131d324fe686
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-06-11 17:57:08 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -8,15 +8,14 @@ let package = Package(
     ],
     products: [
         .library(name: "SpeedsterCore", targets: ["SpeedsterCore"]),
-        .library(name: "SpeedsterApi", targets: ["SpeedsterApi"]),
-        .library(name: "GithubAPI", targets: ["GithubAPI"])
+        .library(name: "SpeedsterApi", targets: ["SpeedsterApi"])
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-alpha"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-alpha"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-alpha"),
-//        .package(url: "https://github.com/shiroyagicorp/swift-seeurl.git", from: "1.9.1"),
+        .package(url: "https://github.com/Einstore/GithubAPI.git", from: "1.0.0"),
         .package(url: "https://github.com/jakeheis/Shout.git", from: "0.5.0"),
         .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.0.0")
     ],
@@ -47,13 +46,6 @@ let package = Package(
                 "SpeedsterCore",
                 "Vapor",
                 "GithubAPI"
-            ]
-        ),
-        .target(
-            name: "GithubAPI",
-            dependencies: [
-                "Vapor",
-                "SwiftShell"
             ]
         ),
         .target(
--- Sources/GithubAPI/Exports.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Exports.swift
-//  
-//
-//  Created by Ondrej Rafaj on 10/06/2019.
-//
-
-@_exported import Vapor
